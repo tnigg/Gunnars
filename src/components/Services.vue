8bbd872601271e30c3a9ffa18ded6d81df8c0110
Thomas Nigg <thomas.nigg@gmx.at>
2021-12-16 22:14:58 +0100

    fixed spacing bug on service cards

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -5,7 +5,7 @@
           <h2 class="pt-1 pb-12 tracking-wide text-center sm:text-lg text-lime-100 md:flex md:justify-evenly">12 zeilen dann kann i karten flippen lassen - besser 8</h2>
           
           <!-- Cards Wrapper -->
-          <div class="w-full px-3 space-y-12 sm:px-8 xl:space-y-3 text-lime-800 lg:flex lg:justify-evenly lg:space-y-0 lg:space-x-12">
+          <div class="w-full px-3 space-y-12 sm:px-8 text-lime-800 lg:flex lg:justify-evenly lg:space-y-0 lg:space-x-12">
 
             <div class="w-full px-4 py-8 space-y-8 border shadow-xl border-lime-700 bg-gradient-to-r from-sky-100 to-gray-200 rounded-xl">
                <h2 class="pb-6 text-xl font-semibold tracking-widest text-center border-b border-lime-700 text-lime-700">Desktop</h2> 
